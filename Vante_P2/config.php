4843770fc4ff2370844666013125340ee2ddca60
TejaswiniS2030 <91017408+TejaswiniS2030@users.noreply.github.com>
2021-09-30 19:22:17 +0530

    Add files via upload

---
+++ Vante_P2/config.php
@@ -0,0 +1,13 @@
+<?php
+	$host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "leafnow_db";
+        $charset = "utf8mb4";
+
+        $conn = mysqli_connect($host, $user, $pass, $db);
+
+	if(!$conn){
+		die("Unable to connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+?>
